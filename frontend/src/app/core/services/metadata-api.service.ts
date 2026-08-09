6904bd91cd4320c94e297712b915a7a07cde1b66
wixhub <rublin@gmx.de>
2026-08-09 22:33:29 +0200

    fix: update environment configuration for production and remove unused environment file

--- frontend/src/app/core/services/metadata-api.service.ts
+++ frontend/src/app/core/services/metadata-api.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpEvent } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { DatasetRecord, IngestionPayload } from '../models/metadata.model';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
